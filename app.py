d1a60187878042861acbfbf9ce6c740875b6243e
LalithaDoddavaram <120888599+LalithaDoddavaram@users.noreply.github.com>
2024-12-14 09:27:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from pymongo import MongoClient
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
-@app.route("/")
+client = MongoClient('mongodb+srv://doddavaramsn27:fbaj1zCmrRBQCtxM@cluster0.howrb.mongodb.net/')
+db = client['sample']
+collection = db['movies']
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "Welcome to the Web page"
+    if request.method == "GET":
+        return render_template("index.html")
+    if request.method == "POST":
+        movie = request.form.get("movie")
+        actor = request.form.get("actor")
+        director = request.form.get("director")
+        year = request.form.get("year")
 
+        collection.insert_one({
+            "movie": movie,
+            "actor": actor,
+            "director": director,
+            "year": year
+        })
+
+        return render_template("result.html", details=list(collection.find({}, {"_id": 0})))
 if __name__ == "__main__":
     app.run(debug=True)
+    
